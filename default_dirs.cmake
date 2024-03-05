7f98e7dee5dd1cec8aa3bf744c8fb46c84828e67
bakedroll <daniel.borchart@protonmail.com>
2024-03-05 17:17:36 +0100

    added default directory config

---
+++ default_dirs.cmake
@@ -0,0 +1,30 @@
+set(GTEST_INCLUDE_DIR "${SDK_BUILD_DIR}/googletest-release-1.8.1/msvc16x64_shared/include" CACHE STRING "")
+
+set(GTEST_BINARY "${SDK_BUILD_DIR}/googletest-release-1.8.1/msvc16x64_shared/googlemock/gtest/Release/gtest.dll" CACHE STRING "")
+set(GTEST_BINARY_DEBUG "${SDK_BUILD_DIR}/googletest-release-1.8.1/msvc16x64_shared/googlemock/gtest/Debug/gtestd.dll" CACHE STRING "")
+set(GTEST_LIBRARY "${SDK_BUILD_DIR}/googletest-release-1.8.1/msvc16x64_shared/googlemock/gtest/Release/gtest.lib" CACHE STRING "")
+set(GTEST_LIBRARY_DEBUG "${SDK_BUILD_DIR}/googletest-release-1.8.1/msvc16x64_shared/googlemock/gtest/Debug/gtestd.lib" CACHE STRING "")
+
+set(GTEST_MAIN_BINARY "${SDK_BUILD_DIR}/googletest-release-1.8.1/msvc16x64_shared/googlemock/gtest/Release/gtest_main.dll" CACHE STRING "")
+set(GTEST_MAIN_BINARY_DEBUG "${SDK_BUILD_DIR}/googletest-release-1.8.1/msvc16x64_shared/googlemock/gtest/Debug/gtest_maind.dll" CACHE STRING "")
+set(GTEST_MAIN_LIBRARY "${SDK_BUILD_DIR}/googletest-release-1.8.1/msvc16x64_shared/googlemock/gtest/Release/gtest_main.lib" CACHE STRING "")
+set(GTEST_MAIN_LIBRARY_DEBUG "${SDK_BUILD_DIR}/googletest-release-1.8.1/msvc16x64_shared/googlemock/gtest/Debug/gtest_maind.lib" CACHE STRING "")
+
+set(LUA_INCLUDE_DIR "${SDK_BUILD_DIR}/lua-5.4.3/msvc16x64/include" CACHE STRING "")
+set(LUA_LIBRARY "${SDK_BUILD_DIR}/lua-5.4.3/msvc16x64/build/Release/lua.lib" CACHE STRING "")
+set(LUA_LIBRARY_DEBUG "${SDK_BUILD_DIR}/lua-5.4.3/msvc16x64/build/Debug/lua.lib" CACHE STRING "")
+
+set(OSG_BINARY_DEBUG "${SDK_BUILD_DIR}/OpenSceneGraph-3.6.4/msvc16x64/bin/osgd.dll" CACHE STRING "")
+set(OSG_BINARY_RELEASE "${SDK_BUILD_DIR}/OpenSceneGraph-3.6.4/msvc16x64/bin/osg.dll" CACHE STRING "")
+set(OSG_LIBRARY_DEBUG "${SDK_BUILD_DIR}/OpenSceneGraph-3.6.4/msvc16x64/lib/osgd.lib" CACHE STRING "")
+set(OSG_LIBRARY_RELEASE "${SDK_BUILD_DIR}/OpenSceneGraph-3.6.4/msvc16x64/lib/osg.lib" CACHE STRING "")
+
+set(OSG_INCLUDE_DIR "${SDK_BUILD_DIR}/OpenSceneGraph-3.6.4/msvc16x64/include" CACHE STRING "")
+set(OSG_PLUGINS_DIRECTORY "${SDK_BUILD_DIR}/OpenSceneGraph-3.6.4/msvc16x64/bin/osgPlugins-3.6.4" CACHE STRING "")
+
+set(OSGPPU_BINARY_DEBUG "${SDK_BUILD_DIR}/osgPPU-3.6.4/msvc16x64/lib/Debug/osgPPUd.dll" CACHE STRING "")
+set(OSGPPU_BINARY_RELEASE "${SDK_BUILD_DIR}/osgPPU-3.6.4/msvc16x64/lib/Debug/osgPPU.dll" CACHE STRING "")
+set(OSGPPU_LIBRARY_DEBUG "${SDK_BUILD_DIR}/osgPPU-3.6.4/msvc16x64/lib/osgPPUd.lib" CACHE STRING "")
+set(OSGPPU_LIBRARY_RELEASE "${SDK_BUILD_DIR}/osgPPU-3.6.4/msvc16x64/lib/osgPPU.lib" CACHE STRING "")
+
+set(OSGPPU_INCLUDE_DIRECTORY "${SDK_BUILD_DIR}/osgPPU-3.6.4/msvc16x64/include" CACHE STRING "")
